e9de2a1e3aea7759580e3d58cbff27f625051949
TRIVARDHAN-17 <tp3615@srmist.edu.in>
2026-02-19 10:31:11 +0530

    welcome message 3

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,7 +1,17 @@
 public class PalindromeCheckerApp {
-    static void main() {
-        System.out.println("Welcome to palindrome checker app Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfulluy");
+
+    public static void main(String[] args) {
+        String original = "madam";
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
     }
 }
+
+
